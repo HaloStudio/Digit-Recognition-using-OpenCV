b42f5f8031eab3bfc608db1656bc7ecb1a9262db
Ranit Raj Ganguly <ranitrajganguly@hotmail.com>
2017-03-19 15:02:05 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,4 +1,4 @@
-# Import the modules
+# Import the required modules
 from sklearn.externals import joblib
 from sklearn import datasets
 from skimage.feature import hog
@@ -6,27 +6,27 @@ from sklearn.svm import LinearSVC
 import numpy as num
 from collections import Counter
 
-# Load the dataset
+# Load the datasets
 dataset = datasets.fetch_mldata("MNIST Original")
 
-# Extract the features and labels
+# Extract the features and labels and storing it in the Numpy Array
 features = num.array(dataset.data, 'int16') 
 labels = num.array(dataset.target, 'int')
 
-# Extract the HOG features
+# Extract the HOG features by creating a list of features and storing it
 list_hog_fd = []
-for feature in features:
-    fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(7, 7), cells_per_block=(1, 1), visualise=True)
-    list_hog_fd.append(fd)
+for ftre in features:
+    featureDescriptor = hog(ftre.reshape((28, 28)), orientations=9, pixels_per_cell=(7, 7), cells_per_block=(1, 1), visualise=True)
+    list_hog_fd.append(featureDescriptor)
 hog_features = num.array(list_hog_fd, 'float64')
 
 print "The Number of digits: ", Counter(labels)
 
-# Create an linear SVM object
+# Create a linear SVM object
 clf = LinearSVC()
 
-# Perform the training
+# Performing the training using the Classifier
 clf.fit(hog_features, labels)
 
-# Save the classifier
+# Saving the classifier
 joblib.dump(clf, "digits_cls.pkl", compress=3)
